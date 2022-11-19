ee4dc9d0a3a86b44e41280e1d66f82e735cd3941
Abominavel_Sirius <mlucas815@gmail.com>
2022-11-19 18:11:26 -0300

    atualizando borda, colisao, movimento

--- sketch.js
+++ sketch.js
@@ -1,9 +1,17 @@
-let xBolinha = 300;
-let yBolinha = 200;
-let diametro = 15;
+// Variaveis da Bolinha ------------------------------------------------------------
+let xBolinha = 300; // vertical
+let yBolinha = 200; // horizontal
+let diametro = 15; // tamanho
+let raio = diametro / 2;
 
-let velocidadeXBolinha = 5
-let velocidadeYBolinha = 5
+// Velocidade da Bolinha --------------------------------------------
+
+let velocidadeXBolinha = 4;
+let velocidadeYBolinha = 4;
+
+// ---------------------------------------------------------------------------------
+
+
 
 function setup() {
   createCanvas(600, 400);
@@ -11,8 +19,27 @@ function setup() {
 
 function draw() {
   background(0);
-  circle(xBolinha,yBolinha,diametro); // circle (x,y,d); 
-  xBolinha += velocidadeXBolinha
-  yBolinha += velocidadeYBolinha
+  mostraBolinha();
+  movimentaBolinha();
+  verificaColisaoBorda();
+}
+
+function mostraBolinha() {
+  circle(xBolinha, yBolinha, diametro); // circle (x,y,d);
+}
+
+function movimentaBolinha() {
+  xBolinha += velocidadeXBolinha;
+  yBolinha += velocidadeYBolinha;
   // += ou seja, X da bolinha será seu valor acrescido de sua velocidade.
+}
+
+function verificaColisaoBorda() {
+  if (xBolinha + raio > width || xBolinha - raio < 0) {
+    velocidadeXBolinha *= -1;
+  }
+
+  if (yBolinha + raio > height || yBolinha - raio < 0) {
+    velocidadeYBolinha *= -1;
+  }
 }
